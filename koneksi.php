882526471ccafd854ce5095b3e6aad9e1eceaa84
Mluweh <54610530+mluweh@users.noreply.github.com>
2019-09-03 20:51:47 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+CRUD
+create-read-update-delete
+
+Data transaksi penjualan 
+
+Tambah baru
+No 	uraian	debet	kredit	aksi
+							hapus edit
+
+
+<?php
+$koneksi = mysqli_connect('localhost:8080', 'root', '', 'htdocs');
